59969c9cc10ac5480bf041fa16d161d96b3b2c49
unknown <justindequillo@gmail.com>
2021-12-06 06:49:35 -0800

    f-commit

---
+++ Arithmetic/variable-scope.py
@@ -0,0 +1,28 @@
+
+# Variable scopes
+x = 7
+print(x)
+print(x-2)
+
+x=10
+print(x)
+
+x = x - 2*5
+print(x)
+
+#-----------------------
+
+
+# Initialize x,y,z variables
+x= 7
+y=-2
+z=5
+
+# 1)Convert to py : 3x (4+y)=42
+
+answer_one = 3*x*(4+y)
+print(answer_one)
+
+# 2)Convert to py : -y- x+3 /z = 0
+answer_two = ( (-y)-(x+3)/z)
+print(answer_two)
