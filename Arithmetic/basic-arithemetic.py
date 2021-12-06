60b9ce5d3f3576b3d717f69de954de924b2b80ca
unknown <justindequillo@gmail.com>
2021-12-06 06:09:02 -0800

    f-commit

---
+++ Arithmetic/basic-arithemetic.py
@@ -0,0 +1,11 @@
+
+
+# Arithmetic order of number test
+
+# 1) 5 - 2/4 * 3/5 = 4.7
+question_one = 5 - (2/4) * (3/5)
+print(question_one)
+
+# 2) 4 - 5 / 3 + 5 x 6 = 0.003
+question_two =(4-5)/(3+5*6)
+print(question_two)
